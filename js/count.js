4d84f4549135fce864890b00deedfb81eed3ad23
Raca <aracki.ivan@gmail.com>
2017-11-10 01:43:15 +0100

    Add getRequest() to golang app

--- js/count.js
+++ js/count.js
@@ -1,20 +1,16 @@
 document.addEventListener("DOMContentLoaded", function (event) {
     // redirect to 7777 port
     // ping golang counter
-    httpGetAsync("http://www.aracki.me:7777/count", function (res) {
-        alert(res);
+    getRequest("localhost:7777/count", function (request) {
+        var response = request.currentTarget.response || request.target.responseText;
+        console.log(response);
     })
 });
 
-function httpGetAsync(theUrl, callback) {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function () {
-        if (xmlHttp.readyState === 4 && xmlHttp.status === 200)
-            callback(xmlHttp.responseText);
-    };
-    xmlHttp.open("GET", theUrl, true); // true for asynchronous
-    xmlHttp.setRequestHeader("Access-Control-Allow-Origin", '*');
-    xmlHttp.setRequestHeader("Access-Control-Allow-Methods", 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
-    xmlHttp.setRequestHeader("Access-Control-Allow-Headers", 'Origin, Content-Type, X-Auth-Token');
-    xmlHttp.send("ivan");
+function getRequest(url, success) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.onload = success;
+    xhr.send();
+    return xhr;
 }
