78151fd873d8f99bcfd9e7dd3bb96cba78ac53cb
Raca <aracki.ivan@gmail.com>
2017-11-10 01:45:58 +0100

    Fix url for get request

--- js/count.js
+++ js/count.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function (event) {
     // redirect to 7777 port
     // ping golang counter
-    getRequest("localhost:7777/count", function (request) {
+    getRequest("http://www.aracki.me:7777/count", function (request) {
         var response = request.currentTarget.response || request.target.responseText;
         console.log(response);
     })
