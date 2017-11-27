37ef3d7cc5c1bd1f6566d84ea7099c169d1eadd4
Raca <aracki.ivan@gmail.com>
2017-11-27 20:08:52 +0100

    Add jquery

---
+++ js/main.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    // redirect to 7777 port
+    // ping golang counter
+    getRequest("http://www.aracki.me:7777/count", function (request) {
+        var response = request.currentTarget.response || request.target.responseText;
+
+        document.getElementById("counter_text")
+            .innerHTML = "[" + response + " unique visitors]";
+    })
+});
+
+$('#yt_text').on('click', function() {
+    //todo NEW PAGE
+})
+
+function getRequest(url, success) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.onload = success;
+    xhr.send();
+    return xhr;
+}
